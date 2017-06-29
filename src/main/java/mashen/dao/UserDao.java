c7fe9d3f5f07829059fddbe9c0b4820199bb9df7
liviluo <1185872070@qq.com>
2017-06-29 09:32:08 +0800

    新增用户

--- src/main/java/mashen/dao/UserDao.java
+++ src/main/java/mashen/dao/UserDao.java
@@ -1,11 +1,22 @@
 package mashen.dao;
 
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import mashen.model.User;
 @Repository("userDao")
 public interface UserDao {
-	public void add(User user);
+	//新增数据
+	public int add(User user);
+	//删除数据
+	public int delete(String accounty);
+	//修改数据
+	public int update(User user);
+	//查询数据
+	public List<User> select(String name);
+	//注册及登陆验证
+	public boolean check(User user);
 
 }
