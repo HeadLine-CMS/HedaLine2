6dc8933792a25a984263c8d3814d6ecc3aff28ab
lasselan <717080748@qq.com>
2017-06-29 16:20:38 +0800

    全句path

--- src/main/java/mashen/action/AdminAction.java
+++ src/main/java/mashen/action/AdminAction.java
@@ -3,6 +3,7 @@ package mashen.action;
 import java.util.List;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,19 +17,20 @@ import mashen.service.InfArticleService;
 @Controller
 @RequestMapping("admin1")
 public class AdminAction {
-	@Resource(name="articleService")
+	@Resource(name = "articleService")
 	private InfArticleService service;
 	@Resource(name = "adminService")
 	private AdminService as;
-//	private AdminUser adminuser = new AdminUser();
+	// private AdminUser adminuser = new AdminUser();
 
-	@RequestMapping("/reg")
+	@RequestMapping("/register")
 	public ModelAndView reg(AdminUser adminuser) {
 		as.AdminAdd(adminuser);
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("admin/register");
 		return mv;
 	}
+
 	@RequestMapping("/update")
 	public ModelAndView update(AdminUser adminuser) {
 		as.AdminUpdate(adminuser);
@@ -36,20 +38,40 @@ public class AdminAction {
 		mv.setViewName("admin/register");
 		return mv;
 	}
+
 	@RequestMapping("/login")
-	public ModelAndView login(String name) {
-//		name="33";
-		List<AdminUser> l=as.AdminSelect(name);
-		for (AdminUser a : l) {
-			System.out.println(a.getPassword()+"="+a.getPassword());
+	public ModelAndView login(HttpServletRequest req,AdminUser user) {
+		if (user.getName() != "" || user.getPassword() != "") {
+			List<AdminUser> l = as.AdminSelect(user.getName());
+			String name = null;
+			String pw = null;
+			for (AdminUser a : l) {
+				name = a.getName();
+				pw = a.getPassword();
+			}
+			System.out.println(name + "/" + pw);
+			if (user.getName().equals(name) && user.getPassword().equals(pw)) {
+				//req.set
+				ModelAndView mv = new ModelAndView();
+				mv.setViewName("admin/register");
+				mv.addObject("success", "成功");
+				return mv;
+			} else {
+				ModelAndView mv = new ModelAndView();
+				mv.setViewName("admin/register");
+				mv.addObject("success", "账号或者密码错误！");
+				return mv;
+			}
+		} else {
+			ModelAndView mv = new ModelAndView();
+			mv.setViewName("admin/register");
+			return mv;
 		}
-		ModelAndView mv = new ModelAndView();
-		mv.setViewName("admin/register");
-		return mv;
 	}
+
 	@RequestMapping("/selectArticle")
 	public ModelAndView selectArticle() {
-		List<Article> l=service.selectArticleByCheck();
+		List<Article> l = service.selectArticleByCheck();
 		for (Article a : l) {
 			System.out.println(a.getTitle());
 		}
@@ -57,9 +79,10 @@ public class AdminAction {
 		mv.setViewName("admin/register");
 		return mv;
 	}
+
 	@RequestMapping("/updateArticle")
 	public ModelAndView updateArticle(String title) {
-		title="ttt";
+		title = "ttt";
 		service.updateArticleByCheck(title);
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("admin/register");
--- src/main/java/mashen/interceptor/Inter.java
+++ src/main/java/mashen/interceptor/Inter.java
@@ -10,6 +10,9 @@ public class Inter extends HandlerInterceptorAdapter {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");
+		request.getServletContext().setAttribute("path",request.getContextPath());
 		System.out.println("初始化");
 		return true;
 	}
